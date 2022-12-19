f977cee34b743760a939519b8a09e3379b4792ee
Piotr <pmanikowski93@gmail.com>
2022-12-19 21:41:20 +0000

    initial commit

---
+++ controlFlowAndConditionals/excercise/src/excercise1.java
@@ -0,0 +1,9 @@
+public class excercise1 {
+    public static void main(String[] args) {
+        boolean isCold = true;
+        if(isCold) {
+            System.out.println("Wear a coat, its cold!");
+
+        }
+    }
+}
